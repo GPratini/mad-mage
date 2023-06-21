9b46276cd112dd8405d461080867143b9578f046
Gabriel Pratini <gabriel.pratini@gmail.com>
2023-06-21 13:40:48 -0300

    Notas salvando e carregando do localStorage

--- src/index.js
+++ src/index.js
@@ -13,22 +13,33 @@ const Mapper = (props) => {
   var [salaAtiva, setSalaAtiva] = useState(null);
   var [nomeSala, setNomeSala] = useState(null);
   var [andarAtivo, setAndarAtivo] = useState(1);
-  var [alturaTextArea, setAlturaTextArea] = useState(null);
   var viewPortWidth = window.innerWidth;
+  const [alturaTextArea, setAlturaTextArea] = useState(null);
+  const [notas, setNotas] = useState("");
   const mapas = [Mapa1, Mapa2];
+  document.title = mapas[andarAtivo - 1].name + ' | Dungeon of the Mad Mage';
 
   function TextAreaHandler(e) {
     setAlturaTextArea(e.target.scrollHeight);
+    setNotas(e.target.value);
+    localStorage.setItem(salaAtiva, e.target.value);
+  }
+
+  function clickedElevador(a) {
+    setAndarAtivo(a);
+    setSalaAtiva(null);
+    setNomeSala(null);
   }
 
   const load = () => {
     setMsg("Interact with the image!");
   };
 
-  const clicked = (area) => {
+  const clickArea = (area) => {
     setMsg(`You clicked on ${area.id} ${area.name}!`);
     setSalaAtiva(area.id);
     setNomeSala(area.name);
+    setNotas(localStorage.getItem(area.id));
   };
 
   const moveOnArea = (area, evt) => {
@@ -48,24 +59,23 @@ const Mapper = (props) => {
   return (
     <div>
       <div id="mapa">
-        <button class='elevador' id='botao1' onClick={() => setAndarAtivo(1)}>1º</button><button class='elevador' id='botao2' onClick={() => setAndarAtivo(2)}>2º</button>
-        {<ImageMapper
-          src={mapas[andarAtivo-1].src}
+        <button className='elevador' id='botao1' onClick={() => clickedElevador(1)}>1º</button><button className='elevador' id='botao2' onClick={() => clickedElevador(2)}>2º</button>
+        <ImageMapper
+          src={mapas[andarAtivo - 1].src}
           map={{
-            name: mapas[andarAtivo-1].name,
-            areas: mapas[andarAtivo-1].areas
+            name: mapas[andarAtivo - 1].name,
+            areas: mapas[andarAtivo - 1].areas
           }}
           onLoad={() => load()}
           onImageClick={(evt) => clickedOutside(evt)}
           onImageMouseMove={(evt) => moveOnImage(evt)}
           onMouseMove={(area, _, evt) => moveOnArea(area, evt)}
-          onClick={(area) => clicked(area)}
-          stayHighlighted= {true}
+          onClick={(area) => clickArea(area)}
+          stayHighlighted={true}
           fillColor='rgba(0, 0, 255, 0.2)'
           responsive={true}
-          parentWidth={(viewPortWidth/100)*48}
-          
-        />}
+          parentWidth={(viewPortWidth / 100) * 48}
+        />
       </div>
       <div id="info">
         <p>{msg ? msg : null}</p>
@@ -73,10 +83,11 @@ const Mapper = (props) => {
 
         <h1>{nomeSala ? nomeSala : "Dungeon of the Mad Mage"}</h1>
 
-        {andarAtivo === 1 && <Salas1 salaAtual={salaAtiva}/>}
-        {andarAtivo === 2 && <Salas2 salaAtual={salaAtiva}/>}
+        {andarAtivo === 1 && <Salas1 salaAtual={salaAtiva} />}
+        {andarAtivo === 2 && <Salas2 salaAtual={salaAtiva} />}
 
-        <textarea style={{height:alturaTextArea-4}} onInput={(e) => TextAreaHandler(e)}></textarea>
+        {salaAtiva && <textarea style={{ height: alturaTextArea - 4 }} value={notas} onChange={e => TextAreaHandler(e)} />}
+        {salaAtiva && <button onClick={() => localStorage.clear()}>Clear all notes</button>}
       </div>
     </div>
   );
@@ -85,7 +96,7 @@ const Mapper = (props) => {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Mapper/>
+    <Mapper />
   </React.StrictMode>
 );
 
